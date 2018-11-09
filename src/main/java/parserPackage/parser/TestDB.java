88529f1e30d640e7f0f471c40c894d6e08bc9283
Alexander Mazanov <zar87liw@gmail.com>
2018-11-09 17:57:29 +0700

    First commit

---
+++ src/main/java/parserPackage/parser/TestDB.java
@@ -0,0 +1,19 @@
+package parserPackage.parser;
+
+public class TestDB {
+    private int id;
+    private String name;
+    private double price;
+    private double weight;
+
+    public TestDB() {
+        id = 0;
+        name = "";
+        price = 0.0;
+        weight = 0.0;
+    }
+
+    public void test() {
+        System.out.println("in test.");
+    }
+}
